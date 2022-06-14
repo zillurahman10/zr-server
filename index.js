ce5c9d2054eeb6a8e2149834ac21d2c8949508ff
zilurahman10 <raihan.brac2019@gmail.com>
2022-06-14 18:23:44 +0600

    f

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const app = express()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cors = require('cors');
+const port = 5000
+
+app.use(express.json())
+app.use(cors())
+// zillur-rahman
+// ZvOjeYf7TrD96fgt
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+
+const uri = "mongodb+srv://zillur-rahman:ZvOjeYf7TrD96fgt@cluster0.jlsdq.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const run = async () => {
+    try {
+        await client.connect()
+
+        const MyProjectsCollection = client.db("zillur-rahman").collection("MyProjects")
+
+        app.get('/myprojects', async (req, res) => {
+            const query = {}
+            const projects = MyProjectsCollection.find(query)
+            const result = await projects.toArray()
+            res.send(result)
+        })
+
+        app.get('/project/:id', async (req, res) => {
+            const _id = req.params.id
+            const query = { id: ObjectId(_id) }
+            const project = await MyProjectsCollection.findOne(query)
+            res.send(project)
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir)
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
